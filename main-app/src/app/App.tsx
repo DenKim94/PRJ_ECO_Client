c204072ff468949c9bf7de5727c68fabfd17a382
Denis Kim <denis-kim@outlook.de>
2026-02-14 16:39:47 +0100

    Initial project set-up done

---
+++ main-app/src/app/App.tsx
@@ -0,0 +1,11 @@
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.appContainer}> 
+      <p>Placeholder</p>
+    </div>
+  );
+}
+
+export default App;
