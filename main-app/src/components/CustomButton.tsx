0853b4d88546eb850b36f1c2b74317e99f28f76e
Denis Kim <denis-kim@outlook.de>
2026-04-12 18:29:23 +0200

    Updated custom button component

--- main-app/src/components/CustomButton.tsx
+++ main-app/src/components/CustomButton.tsx
@@ -1,14 +1,30 @@
 import styles from "./CustomButton.module.scss";
 
+interface IconProps {
+    iconSrc: string;
+    size?: number;
+    alt?: string;
+    ariaLabel?: string;
+    sx?: React.CSSProperties;
+}
+
 interface CustomButtonProps {
     onClickCallback?: () => void | Promise<void>;
     isDisabled?: boolean;
     type?: "button" | "submit" | "reset";
     title: string;
+    iconProps?: IconProps | null;
     sx?: React.CSSProperties;
 }
 
-export const CustomButton = ({ onClickCallback, isDisabled = false, type = "button", title, sx }: CustomButtonProps) => {
+export const CustomButton = ({ 
+    onClickCallback, 
+    isDisabled = false, 
+    type = "button", 
+    title,
+    iconProps = null, 
+    sx }: CustomButtonProps) => {
+
   // Eigener Handler, um den Event-Flow sauber abzufangen
   const handleClick = () => {
       if (onClickCallback) {
@@ -17,12 +33,24 @@ export const CustomButton = ({ onClickCallback, isDisabled = false, type = "butt
   };
 
   return (
-    <button className={styles.customButton} 
-            onClick={handleClick} 
-            type={type} 
-            disabled={isDisabled}
-            style={sx}>
-        {title}
-    </button>
+    <div className={styles.buttonContainer}>
+     {iconProps && 
+        <img 
+            src={iconProps.iconSrc} 
+            alt={iconProps.alt ?? 'Button-Icon'}  
+            width={iconProps.size ?? 24}
+            height={iconProps.size ?? 24} 
+            className={styles.icon}
+            aria-label={iconProps.ariaLabel ?? ''}
+            style={sx}
+        />}   
+        <button className={styles.customButton} 
+                onClick={handleClick} 
+                type={type} 
+                disabled={isDisabled}
+                style={sx}>
+            {title}
+        </button>
+    </div>
   );
 }
